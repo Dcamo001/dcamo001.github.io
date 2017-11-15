e1dac567d901031e3cf34f5c7909c609942b3a94
Daniel Camozzo <dcamo001@gmail.com>
2017-11-15 08:05:24 -0800

    optimized all current photos

--- scripts/main.js
+++ scripts/main.js
@@ -2,19 +2,19 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/Hawaii/echeveria.jpg') {
-      myImage.setAttribute ('src','images/Hawaii/IMG_2486.jpg');
+    if(mySrc === 'images/Hawaii/echeveriaweb.jpg') {
+      myImage.setAttribute ('src','images/Hawaii/IMG_2486web.jpg');
     }
-    if(mySrc === 'images/Hawaii/IMG_2486.jpg') {
-      myImage.setAttribute ('src','images/Hawaii/IMG_2441.jpg');
+    if(mySrc === 'images/Hawaii/IMG_2486web.jpg') {
+      myImage.setAttribute ('src','images/Hawaii/IMG_2441web.jpg');
     }
-    if(mySrc === 'images/Hawaii/IMG_2441.jpg') {
-      myImage.setAttribute ('src','images/Hawaii/IMG_2446.jpg');
+    if(mySrc === 'images/Hawaii/IMG_2441web.jpg') {
+      myImage.setAttribute ('src','images/Hawaii/IMG_2446web.jpg');
     }
-    if(mySrc === 'images/Hawaii/IMG_2446.jpg') {
-      myImage.setAttribute ('src','images/Hawaii/IMG_2462.jpg');
+    if(mySrc === 'images/Hawaii/IMG_2446web.jpg') {
+      myImage.setAttribute ('src','images/Hawaii/IMG_2462web.jpg');
     }
-    if(mySrc === 'images/Hawaii/IMG_2462.jpg') {
-      myImage.setAttribute ('src','images/Hawaii/echeveria.jpg');
+    if(mySrc === 'images/Hawaii/IMG_2462web.jpg') {
+      myImage.setAttribute ('src','images/Hawaii/echeveriaweb.jpg');
     }
 }
