52e24827e0e812a0466afbe30dd4d000d69fbd38
Daniel Camozzo <dcamo001@gmail.com>
2017-11-14 16:48:35 -0800

    Image scaling done

--- scripts/main.js
+++ scripts/main.js
@@ -2,19 +2,19 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/echeveria.jpg') {
-      myImage.setAttribute ('src','images/IMG_2486.jpg');
+    if(mySrc === 'images/Hawaii/echeveria.jpg') {
+      myImage.setAttribute ('src','images/Hawaii/IMG_2486.jpg');
     }
-    if(mySrc === 'images/IMG_2486.jpg') {
-      myImage.setAttribute ('src','images/IMG_2441.jpg');
+    if(mySrc === 'images/Hawaii/IMG_2486.jpg') {
+      myImage.setAttribute ('src','images/Hawaii/IMG_2441.jpg');
     }
-    if(mySrc === 'images/IMG_2441.jpg') {
-      myImage.setAttribute ('src','images/IMG_2446.jpg');
+    if(mySrc === 'images/Hawaii/IMG_2441.jpg') {
+      myImage.setAttribute ('src','images/Hawaii/IMG_2446.jpg');
     }
-    if(mySrc === 'images/IMG_2446.jpg') {
-      myImage.setAttribute ('src','images/IMG_2462.jpg');
+    if(mySrc === 'images/Hawaii/IMG_2446.jpg') {
+      myImage.setAttribute ('src','images/Hawaii/IMG_2462.jpg');
     }
-    if(mySrc === 'images/IMG_2462.jpg') {
-      myImage.setAttribute ('src','images/echeveria.jpg');
+    if(mySrc === 'images/Hawaii/IMG_2462.jpg') {
+      myImage.setAttribute ('src','images/Hawaii/echeveria.jpg');
     }
 }
