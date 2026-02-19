bfeba2f0c0ac888714d3ad5dc0d9c10d9c13a45d
Danny <dcamo001@gmail.com>
2026-02-18 22:53:24 -0500

    Improvements from Cursor Ai audit v1

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,34 @@
-//(function JStest() {
-//    console.log("hello!");
-//})();
-//
-//jQuery.noConflict();
+(function () {
+  "use strict";
 
-jQuery(document).ready(function() {
-  jQuery(".toggle-nav").click(function(e) {
-    jQuery(this).toggleClass("active");
-    jQuery(".mobileMenubar").toggleClass("active");
-    e.preventDefault();
-  });
-});
-
-$(document).ready(function() {
-  $("[href]").each(function() {
-    if (this.href == window.location.href) {
-      $(this).addClass("active");
+  function ready(fn) {
+    if (document.readyState !== "loading") {
+      fn();
+    } else {
+      document.addEventListener("DOMContentLoaded", fn);
     }
+  }
+
+  ready(function () {
+    var toggle = document.querySelector(".toggle-nav");
+    var mobileMenu = document.querySelector(".mobileMenubar");
+
+    if (toggle && mobileMenu) {
+      toggle.addEventListener("click", function (e) {
+        e.preventDefault();
+        var expanded = this.getAttribute("aria-expanded") === "true";
+        this.setAttribute("aria-expanded", !expanded);
+        this.classList.toggle("active");
+        mobileMenu.classList.toggle("active");
+      });
+    }
+
+    var currentHref = window.location.href.split("?")[0].split("#")[0];
+    document.querySelectorAll("a[href]").forEach(function (link) {
+      var linkHref = link.href.split("?")[0].split("#")[0];
+      if (linkHref === currentHref) {
+        link.classList.add("active");
+      }
+    });
   });
-});
-
-//    $(".gallery").on('click',function(){
-//        //removes enlarged class from all images
-//        $(".gallery.enlarged").removeClass('enlarged');
-//        //adds enlarged class to clicked image
-//        $(this).addClass('enlarged');
-//    });
-
-//    $(function ()
-//{
-//    $('img').on('click', function ()
-//    {
-//        $(this).width(1000);
-//    });
-//});
+})();
